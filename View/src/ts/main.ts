3b46dd815574c92b929fddf560cdc0d9ed6817b0
pruiz5514 <pablo_ruiza@hotmail.com>
2024-07-25 18:00:44 -0500

    feat: form in index.html is created

--- View/src/ts/main.ts
+++ View/src/ts/main.ts
@@ -1,3 +1,8 @@
 // Import our custom CSS
 import '../scss/style.scss';
 
+const form = document.querySelector("form") as HTMLFormElement;
+
+form.addEventListener("submit", (event: Event) => {
+    event.preventDefault();
+})
