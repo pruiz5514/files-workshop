13470a0ee519024f2bcbebaf2809581f99617f13
pruiz5514 <pablo_ruiza@hotmail.com>
2024-07-26 09:47:34 -0500

    feat: pagination is done

--- Controllers/File.controller.ts
+++ Controllers/File.controller.ts
@@ -5,31 +5,40 @@ export class FileController {
     tableContainer: HTMLElement;
     initialElement: number;
     finalElement: number;
+    array: dataFile | null;
+
 
     constructor(input: HTMLInputElement, tableContainer: HTMLElement, initialElement: number, finalElement: number) {
-        this.input = input,
-            this.tableContainer = tableContainer;
+        this.input = input;
+        this.tableContainer = tableContainer;
         this.initialElement = initialElement;
         this.finalElement = finalElement;
+        this.array = null;
+
     }
 
-    getData(): void {
-        let fileReader = new FileReader();
+    getData(): Promise<void> {
+        return new Promise((resolve) => {
+            if (this.input.files && this.input.files.length === 1) {
+                const file = this.input.files[0];
+                const fileReader = new FileReader();
 
-        if (this.input.files && this.input.files.length === 1) {
-            const file = this.input.files[0];
+                fileReader.onload = (event) => {
+                    const text = event.target?.result as string;
 
-            fileReader.onload = (event) => {
-                const text = event.target?.result as string;
+                    const rows = text?.split("\n");
+                    this.array = rows.map(row => row.split(","));
+                    console.log(this.array.length);
 
-                const rows = text?.split("\n");
-                const array = rows.map(row => row.split(","))
+                    this.tableContainer.innerHTML = "";
+                    this.tableContainer.append(this.createTable(this.array));
 
-                this.tableContainer.innerHTML = "";
-                this.tableContainer.appendChild(this.createTable(array));
+                    resolve();
+                };
+
+                fileReader.readAsText(file);
             }
-            fileReader.readAsText(file)
-        }
+        });
     }
     createTable(array: dataFile) {
         const table = document.createElement("table") as HTMLTableElement;
@@ -46,7 +55,7 @@ export class FileController {
 
         const tbody = document.createElement("tbody") as HTMLTableSectionElement;
         array.forEach((row, index) => {
-            if (index !== 0 && index > this.initialElement && index <= this.finalElement) {
+            if (index !== 0 && index >= this.initialElement && index <= this.finalElement) {
                 let tr = document.createElement("tr") as HTMLTableCaptionElement;
 
                 row.forEach(cell => {
@@ -65,5 +74,8 @@ export class FileController {
         return table;
     }
 
+    async getArrayLength() {
+        return await this.array?.length;
+    }
 
 }
--- View/src/ts/main.ts
+++ View/src/ts/main.ts
@@ -4,39 +4,53 @@ import { FileController } from '../../../Controllers/File.controller'
 const form = document.querySelector("form") as HTMLFormElement;
 const input = document.querySelector("#file-input") as HTMLInputElement;
 const tableContainer = document.querySelector(".table-container") as HTMLElement;
-
-
-
-let initialElement = 1;
-let finalElement = 15;
-
-
-form.addEventListener("submit", (event: Event) => {
-    event.preventDefault();
-
-    const fileController = new FileController(input, tableContainer, initialElement, finalElement);
-    fileController.getData();
-
-})
-
 const nextButton = document.querySelector("#next-button") as HTMLButtonElement;
 const backButton = document.querySelector("#back-button") as HTMLButtonElement;
 
-nextButton.addEventListener("click", () => {
-    initialElement += 15;
-    finalElement += 15;
 
-    const fileController = new FileController(input, tableContainer, initialElement, finalElement);
-    fileController.getData();
+const pagination = 15
+let initialElement = 1;
+let finalElement = pagination;
+
+
+let fileController = new FileController(input, tableContainer, initialElement, finalElement);
+
+async function updateData() {
+    await fileController.getData();
+    const arrayLength = fileController.array?.length ?? 0;
+
+    if (finalElement < arrayLength) {
+        nextButton.style.display = "inline";
+    } else {
+        nextButton.style.display = "none";
+    }
+
+    if (initialElement <= pagination) {
+        backButton.style.display = "none";
+    } else {
+        backButton.style.display = "inline";
+    }
+}
+
+form.addEventListener("submit", async (event: Event) => {
+    event.preventDefault();
+    fileController = new FileController(input, tableContainer, initialElement, finalElement);
+    await updateData()
 })
 
-backButton.addEventListener("click", () => {
-    if (initialElement >= 15) {
-        initialElement -= 15;
-        finalElement -= 15;
+nextButton.addEventListener("click", async () => {
+    initialElement += pagination;
+    finalElement += pagination;
+    fileController = new FileController(input, tableContainer, initialElement, finalElement);
+    await updateData();
+});
 
-        const fileController = new FileController(input, tableContainer, initialElement, finalElement);
-        fileController.getData();
+backButton.addEventListener("click", async () => {
+    if (initialElement > pagination) {
+        initialElement -= pagination;
+        finalElement -= pagination;
+        fileController = new FileController(input, tableContainer, initialElement, finalElement);
+        await updateData();
     }
 })
 
